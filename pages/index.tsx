1514dbe557a9a1583091fa17bdb98c5036c735f1
Luke <lukepbayliss@gmail.com>
2020-03-10 22:09:28 +1100

    adds emotion

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,50 @@
 import * as React from 'react';
 import { NextPage } from 'next';
+import styled from '@emotion/styled';
+import { Global, css, keyframes } from '@emotion/core';
 
-type Props = {
-  title: string;
+const bounce = keyframes`
+  from, 20%, 53%, 80%, to {
+    transform: translate3d(0,0,0);
+  }
+
+  40%, 43% {
+    transform: translate3d(0, -30px, 0);
+  }
+
+  70% {
+    transform: translate3d(0, -15px, 0);
+  }
+
+  90% {
+    transform: translate3d(0,-4px,0);
+  }
+`;
+
+const Title = styled.h1<{ textColor: string }>`
+  color: ${props => props.textColor};
+  animation: ${bounce} 1s ease infinite;
+`;
+
+const IndexPage: NextPage = () => {
+  return (
+    <>
+      <Global
+        styles={css`
+          html,
+          body {
+            padding: 3rem 1rem;
+            margin: 0;
+            background: papayawhip;
+            min-height: 100%;
+            font-family: Helvetica, Arial, sans-serif;
+            font-size: 24px;
+          }
+        `}
+      />
+      <Title textColor="red">Hello World!</Title>
+    </>
+  );
 };
-
-const IndexPage: NextPage<Props> = ({ title }) => <div>Hello World! {title}</div>;
 
 export default IndexPage;
