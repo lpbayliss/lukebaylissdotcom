bb25c02d2f03c778c997e4533e42a9163d269935
Luke <lukepbayliss@gmail.com>
2020-03-12 19:42:33 +1100

    adds syntax highlighting

--- src/components/code-block/code-block.component.tsx
+++ src/components/code-block/code-block.component.tsx
@@ -1,13 +1,44 @@
-import * as React from 'react';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { darcula as style } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+// src/components/CodeBlock.js
+import React from 'react';
+import Highlight, { defaultProps, Language } from 'prism-react-renderer';
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
 
-type Props = { value: string; language?: string };
+type Props = {
+  children: string;
+  className: string;
+  live: boolean;
+};
 
-const CodeBlock: React.FunctionComponent<Props> = ({ value, language = null }) => (
-  <SyntaxHighlighter language={language} style={style}>
-    {value}
-  </SyntaxHighlighter>
-);
+const CodeBlock: React.FunctionComponent<Props> = ({ children, className, live }) => {
+  const language = className.replace(/language-/, '') as Language;
+
+  if (live) {
+    return (
+      <div style={{ marginTop: '40px' }}>
+        <LiveProvider code={children}>
+          <LivePreview />
+          <LiveEditor />
+          <LiveError />
+        </LiveProvider>
+      </div>
+    );
+  }
+
+  return (
+    <Highlight {...defaultProps} code={children} language={language}>
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={{ ...style, padding: '20px' }}>
+          {tokens.map((line, i) => (
+            <div key={i} {...getLineProps({ line, key: i })}>
+              {line.map((token, key) => (
+                <span key={key} {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  );
+};
 
 export default CodeBlock;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import * as React from 'react';
 import { AppProps } from 'next/app';
+import { MDXProvider } from '@mdx-js/react';
+import CodeBlock from '../components/code-block/code-block.component';
+
+const components = {
+  code: CodeBlock,
+};
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <MDXProvider components={components}>
+      <Component {...pageProps} />
+    </MDXProvider>
+  );
 }
 
 export default MyApp;
