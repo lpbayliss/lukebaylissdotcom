42348a32d8abe2b58fb061d989b69fd1f3b9b1d2
Luke <lukepbayliss@gmail.com>
2020-03-12 19:53:35 +1100

    reset styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,27 @@
 import * as React from 'react';
 import { AppProps } from 'next/app';
 import { MDXProvider } from '@mdx-js/react';
+import { Global, css } from '@emotion/core';
+import emotionNormalize from 'emotion-normalize';
+
 import CodeBlock from '../components/code-block/code-block.component';
 
 const components = {
   code: CodeBlock,
 };
 
+const StyleReset = (): JSX.Element => (
+  <Global
+    styles={css`
+      ${emotionNormalize}
+    `}
+  />
+);
+
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <MDXProvider components={components}>
+      <StyleReset />
       <Component {...pageProps} />
     </MDXProvider>
   );
