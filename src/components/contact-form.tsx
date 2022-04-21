3cfb3cb8f1d1e1ebbb0ea26bdf0c711dca6fafaf
Luke Bayliss <lukepbayliss@gmail.com>
2022-04-22 01:18:35 +1000

    hail mary

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -11,44 +11,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ContactForm = () => {
   return (
-    <form name="contact" action="/success" method="POST" data-netlify="true">
-      <Input type="hidden" name="form-name" value="contact" />
-      <Stack spacing={4}>
-        <Stack direction={{ base: "column", md: "row" }}>
-          <FormControl>
-            <FormLabel htmlFor="name">Your Name</FormLabel>
-            <Input id="name" type="text" />
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor="email">Email address</FormLabel>
-            <Input id="email" type="email" />
-          </FormControl>
-        </Stack>
-        <FormControl>
-          <FormLabel htmlFor="subject">Subject</FormLabel>
-          <Input id="subject" type="text" />
-        </FormControl>
-        <FormControl>
-          <FormLabel htmlFor="body">Your Message</FormLabel>
-          <Textarea
-            id="body"
-            size="lg"
-            resize="none"
-            minH="3xs"
-            placeholder="What did the fish say when he swam into a wall? Dam."
-          />
-        </FormControl>
-        <FormControl>
-          <Button
-            w="full"
-            leftIcon={<FontAwesomeIcon icon={faPaperPlane} />}
-            type="submit"
-            size="lg"
-          >
-            Send
-          </Button>
-        </FormControl>
-      </Stack>
+    <form name="contact" method="POST" data-netlify="true">
+      <input type="hidden" name="form-name" value="contact" />
+      <p>
+        <label>
+          Your Name: <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email: <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message: <textarea name="message"></textarea>
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
     </form>
   );
 };
