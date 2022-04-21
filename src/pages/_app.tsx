e81e9215a8e096bea124c2b942c8b4b25ea08361
Luke Bayliss <lukepbayliss@gmail.com>
2022-04-21 23:38:27 +1000

    revert changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,15 +4,12 @@ import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
 import theme from "../lib/theme";
-import ColorModeButton from "../components/color-mode-button";
 
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme}>
-      <ColorModeButton />
-
       <Component {...pageProps} />
     </ChakraProvider>
   );
